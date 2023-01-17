46c72c7b471a309ce1a05ea5aa8c0e9852d8c2e8
Catalin Banici <catalinbanici95@yahoo.com>
2023-01-17 17:19:09 +0100

    added validations

--- app/src/App.js
+++ app/src/App.js
@@ -2,10 +2,11 @@ import MainDesktop from './Assets/images/bg-main-desktop.png';
 import CardFront from './Assets/images/bg-card-front.png';
 import CardBack from './Assets/images/bg-card-back.png';
 import { useFormik } from 'formik';
+import { schema } from './Schema/ValidationSchema';
 
 function App() {
 
-  const { values, handleChange, handleBlur } = useFormik({
+  const { values, errors, touched, handleChange, handleBlur, handleSubmit } = useFormik({
     initialValues: {
       cardName: '',
       cardNumber: '',
@@ -13,21 +14,28 @@ function App() {
       expDateY: '',
       cvc: ''
     },
+    validationSchema: schema,
+    onSubmit
   });
 
+  function onSubmit() {
+    console.log('Submitted!')
+  }
+
   console.log(values)
+  // console.log(errors)
 
   return (
     <div className="app-container">
       <header className='header-container'>
         <img className='main-image' src={MainDesktop} alt='main' />
         <div className='card-img-container'>
-          <span className='on-card-name'></span>
-          <span className='on-card-number'></span>
-          <span className='on-card-exp-m'></span>
-          <span className='slash'></span>
-          <span className='on-card-exp-y'></span>
-          <span className='cvc-on-card'></span>
+          <span className='on-card-name'>{values.cardName || 'Jane Appleseed'}</span>
+          <span className='on-card-number'>{values.cardNumber || '0000 0000 0000 0000'}</span>
+          <span className='on-card-exp-m'>{values.expDateM || '00'}</span>
+          <span className='slash'>/</span>
+          <span className='on-card-exp-y'>{values.expDateY || '00'}</span>
+          <span className='cvc-on-card'>{values.cvc || '000'}</span>
           <svg className='card-logo' width="84" height="47" fill="none" xmlns="http://www.w3.org/2000/svg">
             <ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff"/>
             <path d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z" stroke="#fff"/>
@@ -37,35 +45,35 @@ function App() {
         </div>
       </header>
       
-      <form className='form-container' >
+      <form className='form-container' onSubmit={handleSubmit} >
         <label className='cardholder-name label'>
           CARDHOLDER NAME
           <input 
-            className='input-cardholder-name'
+            className='input-cardholder-name' 
+            style={errors.cardName && touched.cardName && {border: '1px solid red'}}
             type='text' 
             id='cardName'
-            required 
             placeholder='e.g. Jane Appleseed'
             value={values.cardName}
             onChange={handleChange}
             onBlur={handleBlur}
           />
-          <span className='error-message-name'></span>
+          {errors.cardName && touched.cardName && <span className='error-message'>{errors.cardName}</span>}
         </label>
 
         <label className='card-number label'>
           CARD NUMBER
           <input 
             className='input-card-number'
+            style={errors.cardNumber && touched.cardNumber && {border: '1px solid red'}}
             type='text' 
             id='cardNumber' 
-            required 
             placeholder='e.g. 1234 5678 9123 0000'
             value={values.cardNumber}
             onChange={handleChange}
             onBlur={handleBlur}
           />
-          <span className='error-message-number'></span>
+          {errors.cardNumber && touched.cardNumber && <span className='error-message'>{errors.cardNumber}</span>}
         </label>
         
         <div className='exp-cvc-container'>
@@ -74,9 +82,9 @@ function App() {
             <div>
               <input 
                 className='input-exp-date-m'
+                style={errors.expDateM && touched.expDateM && {border: '1px solid red'}}
                 type='text'  
                 id='expDateM'
-                required 
                 placeholder='MM'
                 value={values.expDateM}
                 onChange={handleChange}
@@ -84,15 +92,15 @@ function App() {
               />
               <input 
                 className='input-exp-date-y'
+                style={errors.expDateY && touched.expDateY && {border: '1px solid red'}}
                 type='text' 
                 id='expDateY'
-                required 
                 placeholder='YY'
                 value={values.expDateY}
                 onChange={handleChange}
                 onBlur={handleBlur}
               /> 
-              <span className='error-message-exp'></span> 
+              {errors.expDateM && errors.expDateY && touched.expDateM && touched.expDateY &&  <span className='error-message error-message-exp'>{errors.expDateM && errors.expDateY}</span>} 
             </div>       
           </label>
           
@@ -100,15 +108,15 @@ function App() {
               CVC
               <input
                 className='input-cvc'
+                style={errors.cvc && touched.cvc && {border: '1px solid red'}}
                 type='text'
                 id='cvc' 
-                required  
                 placeholder='123'
                 value={values.cvc}
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
-              <span className='error-message-cvc'></span>
+              {errors.cvc && touched.cvc && <span className='error-message'>{errors.cvc}</span>}
           </label>       
         </div>
         <button>Confirm</button>
