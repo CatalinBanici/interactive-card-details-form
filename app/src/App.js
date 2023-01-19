65812b5311cce6dceba13799e35d515a522a5b0c
Catalin Banici <catalinbanici95@yahoo.com>
2023-01-19 20:24:42 +0100

    added routes and thank you component

--- app/src/App.js
+++ app/src/App.js
@@ -1,40 +1,61 @@
-
-import { useFormik } from 'formik';
-import { schema } from './Schema/ValidationSchema';
-import Header from './Components/Header';
-import Form from './Components/Form';
+import { useFormik } from "formik";
+import { schema } from "./Schema/ValidationSchema";
+import Header from "./Components/Header";
+import Form from "./Components/Form";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Complete from "./Components/Complete";
 
 function App() {
-
-  const { values, errors, touched, isSubmitting, handleChange, handleBlur, handleSubmit } = useFormik({
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = useFormik({
     initialValues: {
-      cardName: '',
-      cardNumber: '',
-      expDateM: '',
-      expDateY: '',
-      cvc: ''
+      cardName: "",
+      cardNumber: "",
+      expDateM: "",
+      expDateY: "",
+      cvc: "",
     },
     validationSchema: schema,
-    onSubmit
+    onSubmit,
   });
 
-  async function onSubmit() {
+  const navigate = useNavigate();
+
+  async function onSubmit(values, actions) {
     await new Promise((resolve) => setTimeout(resolve, 2000));
-    console.log(values)
+    console.log(values);
+    navigate("/Complete");
+    actions.resetForm();
   }
 
   return (
     <div className="app-container">
       <Header values={values} />
-      <Form 
-        values={values} 
-        errors={errors} 
-        touched={touched} 
-        isSubmitting={isSubmitting} 
-        handleChange={handleChange} 
-        handleBlur={handleBlur}
-        handleSubmit={handleSubmit} 
-        onSubmit={onSubmit} />     
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Form
+              values={values}
+              errors={errors}
+              touched={touched}
+              isSubmitting={isSubmitting}
+              handleChange={handleChange}
+              handleBlur={handleBlur}
+              handleSubmit={handleSubmit}
+              onSubmit={onSubmit}
+            />
+          }
+        />
+        <Route path="/Complete" element={<Complete />} />
+      </Routes>
     </div>
   );
 }
--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -1,27 +1,41 @@
-import React from 'react';
-import MainDesktop from '../Assets/images/bg-main-desktop.png';
-import CardFront from '../Assets/images/bg-card-front.png';
-import CardBack from '../Assets/images/bg-card-back.png';
+import React from "react";
+import MainDesktop from "../Assets/images/bg-main-desktop.png";
+import CardFront from "../Assets/images/bg-card-front.png";
+import CardBack from "../Assets/images/bg-card-back.png";
 
 export default function Header(props) {
+  const { values } = props;
 
-    const {values} = props
-    
-    return (
-        <header className='header-container'>
-            <img className='main-image' src={MainDesktop} alt='main' />
-            <div className='card-img-container'>
-                <span className='on-card-name'>{values.cardName || 'Jane Appleseed'}</span>
-                <span className='on-card-number'>{values.cardNumber || '0000 0000 0000 0000'}</span>
-                <span className='on-card-exp'>{values.expDateM || '00'}/{values.expDateY || '00'}</span>
-                <span className='cvc-on-card'>{values.cvc || '000'}</span>
-                <svg className='card-logo' width="84" height="47" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff"/>
-                    <path d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z" stroke="#fff"/>
-                </svg>
-                <img className='card-front' src={CardFront} alt='front card' />
-                <img className='card-back' src={CardBack} alt='back card' />   
-            </div>
-        </header>
-  )
+  return (
+    <header className="header-container">
+      <img className="main-image" src={MainDesktop} alt="main" />
+      <div className="card-img-container">
+        <span className="on-card-name">
+          {values.cardName || "Jane Appleseed"}
+        </span>
+        <span className="on-card-number">
+          {values.cardNumber || "0000 0000 0000 0000"}
+        </span>
+        <span className="on-card-exp">
+          {values.expDateM || "00"}/{values.expDateY || "00"}
+        </span>
+        <span className="cvc-on-card">{values.cvc || "000"}</span>
+        <svg
+          className="card-logo"
+          width="84"
+          height="47"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff" />
+          <path
+            d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z"
+            stroke="#fff"
+          />
+        </svg>
+        <img className="card-front" src={CardFront} alt="front card" />
+        <img className="card-back" src={CardBack} alt="back card" />
+      </div>
+    </header>
+  );
 }
