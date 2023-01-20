522493cb5cb209202602f05ab45cf8e75aa2654e
Catalin Banici <catalinbanici95@yahoo.com>
2023-01-20 19:16:10 +0100

    finished styiling and media queries

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import MainDesktop from "../Assets/images/bg-main-desktop.png";
 import CardFront from "../Assets/images/bg-card-front.png";
 import CardBack from "../Assets/images/bg-card-back.png";
 
@@ -20,19 +19,7 @@ export default function Header(props) {
           {values.expDateM || "00"}/{values.expDateY || "00"}
         </span>
         <span className="on-card-cvc">{values.cvc || "000"}</span>
-        <svg
-          className="card-logo"
-          width="84"
-          height="47"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff" />
-          <path
-            d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z"
-            stroke="#fff"
-          />
-        </svg>
+        <svg className="card-logo"></svg>
         <img className="card-front" src={CardFront} alt="front card" />
         <img className="card-back" src={CardBack} alt="back card" />
       </div>
